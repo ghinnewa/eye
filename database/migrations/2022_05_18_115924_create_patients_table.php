ca6a6268aec3cd0d3a048e3fbab8f933653034c5
ghinnewa <hajer.ghinnewah@gmail.com>
2022-05-18 13:23:33 +0100

    first in project

---
+++ database/migrations/2022_05_18_115924_create_patients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('national_number');
+            $table->string('gender');
+            $table->string('age');
+            $table->string('case');
+            $table->integer('hospital_id');
+            $table->string('phone');
+            $table->string('diagnosis');
+            $table->string('desession');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+};
